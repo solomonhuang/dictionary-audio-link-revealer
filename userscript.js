d3b6c95058018ba7cf6c20c4b6a0dcf083e4b3ee
Solomon Huang <kaichanh@gmail.com>
2019-10-28 18:35:26 +0800

    remove jquery

--- userscript.js
+++ userscript.js
@@ -1,22 +1,29 @@
 // ==UserScript==
 // @name         Dictionary audio link revealer
-// @namespace    solomonhuang-dalr
-// @version      0.0.1-20191019
-// @description  try to take over the world!
+// @namespace    https://github.com/solomonhuang/dictionary-audio-link-revealer
+// @version      0.0.2-20191028
+// @description  Add audio file link of dictionary pages
 // @author       Solomon Huang
 // @match        https://dictionary.cambridge.org/dictionary/*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @updateURL    https://raw.githubusercontent.com/solomonhuang/dictionary-audio-link-revealer/master/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/solomonhuang/dictionary-audio-link-revealer/master/userscript.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Your code here...
-    $('audio source[type="audio/mpeg"]').each(function(index) {
-        var src = $( this ).attr('src')
-        $(this).parent().parent().siblings().append('<a style="text-decoration:none" href="' + src + '">&#x1f517;</a>')
-        //console.log( index + ": " + src)
-    })
+    var daud = document.getElementsByClassName('daud')
+    for (var i = 0; i < daud.length; i++) {
+        var a = daud[i].getElementsByTagName('audio')
+        var src = a[0].children[0].src
+        var link = document.createElement('a')
+        link.href = src
+        link.style = 'text-decoration:none'
+        link.appendChild(document.createTextNode('&#x1F517;'))
+        daud[i].children[1].append(link)
+    }
+
 })();
 
